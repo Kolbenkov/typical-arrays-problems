166f4af600c80c4f184109260169cfb544d17ff8
Kolbenkov <55102046+Kolbenkov@users.noreply.github.com>
2021-01-30 19:25:10 +0300

    fix: fixed bugs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 
 exports.min = function min (array) {
   return Math.min.apply(Math, array);
+
 }
 
 exports.max = function max (array) {
-  return Math.max.apply(Math, obj);
+  return Math.max.apply(Math, array);
+
 }
 
 exports.avg = function avg (array) {
-    var total = 0;
-for(var i = 0; i < grades.length; i++) {
-    total += grades[i];
+  var total = 0;
+for(var i = 0; i < array.length; i++) {
+    total += array[i];
 }
-var avg = total / grades.length;
+var avg = total / array.length;
   return avg;
 }
